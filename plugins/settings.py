9f5ca049f0dd76803757380e3edfe02357bc6e1d
MonarchXshubham <shubhamxmonarch@gmail.com>
2024-10-20 13:00:31 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -219,7 +219,7 @@ async def settings_query(bot, query):
      buttons.append([InlineKeyboardButton('↩ Back', 
                       callback_data="settings#main")])
      await query.message.edit_text(
-        "<b><u>🔘CUSTOM BUTTON</b></u>\n\n<b>You can add inline buttons to messages with the following format:</b>\n\n<b>Single Button in a row:</b>\n\n`[Forward bot][buttonurl:https://t.me/Jisshu_forward_bot]`\n\n<b>More than one button in same row:</b>\n\n`[forward bot][buttonurl:https://t.me/Jisshu_forward_bot]\n[forward bot][buttonurl:https://t.me/Jisshu_forward_bot(:same)]`",
+        "<b><u>🔘CUSTOM BUTTON</b></u>\n\n<b>You can add inline buttons to messages with the following format:</b>\n\n<b>Single Button in a row:</b>\n\n`[Forward bot][buttonurl:https://t.me/VR_Forward_Bot]`\n\n<b>More than one button in same row:</b>\n\n`[forward bot][buttonurl:https://t.me/VR_Forward_Bot]\n[forward bot][buttonurl:https://t.me/VR_Forward_Bot(:same)]`",
         reply_markup=InlineKeyboardMarkup(buttons))
   
   elif type=="addbutton":
